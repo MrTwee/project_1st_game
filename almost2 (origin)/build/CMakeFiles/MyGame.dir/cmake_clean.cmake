4bd8b7b2572589e53164c87770dfe2b6c9f7ebd7
kukusum <zdrgnde@gmail.com>
2024-03-22 14:07:03 +0900

    build

---
+++ almost2 (origin)/build/CMakeFiles/MyGame.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyGame.dir/game.c.o"
+  "CMakeFiles/MyGame.dir/game.c.o.d"
+  "CMakeFiles/MyGame.dir/main.c.o"
+  "CMakeFiles/MyGame.dir/main.c.o.d"
+  "CMakeFiles/MyGame.dir/utility.c.o"
+  "CMakeFiles/MyGame.dir/utility.c.o.d"
+  "MyGame"
+  "MyGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MyGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
