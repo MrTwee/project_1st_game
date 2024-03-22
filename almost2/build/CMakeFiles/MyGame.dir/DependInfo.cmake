c7bf8d70f660420e50ee347b253f6d0249914266
kukusum <160800347+kukusum@users.noreply.github.com>
2024-03-22 11:32:14 +0900

    Add files via upload

---
+++ almost2/build/CMakeFiles/MyGame.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kukusu/Downloads/project_1st_game-main/almost1/game.c" "CMakeFiles/MyGame.dir/game.c.o" "gcc" "CMakeFiles/MyGame.dir/game.c.o.d"
+  "/home/kukusu/Downloads/project_1st_game-main/almost1/main.c" "CMakeFiles/MyGame.dir/main.c.o" "gcc" "CMakeFiles/MyGame.dir/main.c.o.d"
+  "/home/kukusu/Downloads/project_1st_game-main/almost1/utility.c" "CMakeFiles/MyGame.dir/utility.c.o" "gcc" "CMakeFiles/MyGame.dir/utility.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
