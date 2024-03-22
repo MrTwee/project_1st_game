1ff43171e389095c0e6995bad866bd395be04b86
kukusum <zdrgnde@gmail.com>
2024-03-22 12:50:02 +0900

    거의완료

--- almost2 (사본)/utility.c
+++ almost2 (사본)/utility.c
@@ -23,46 +23,49 @@ void enableRawMode() {
 
 //크레딧 출력 로직
 void display_credits() {
-	printf("\033[2J\033[H"); // Clear screen
-	
-	printf("\n");
-	printf("\n");
-	printf("　　　　　　　　        Sola HONG　Data Manager\n");
-	sleep(1);
-	printf("　　　　　　　　　　　　　　　　 　Subtitle/Translate\n");
-	sleep(1);
-	printf("　　　　　　　　　　　　　　　 　　Line Producer\n");
-	sleep(1);
-	printf("\n");
-	
-	printf("　　　　　　　　　　 Haedeun SONG　Production Assistant\n");
-	sleep(1);
-	printf("　　　　　　　　　　　　　　　 　　Art Design\n");
-	sleep(1);
-	printf("　　　　　　　　　　　　　　　 　　Tech Engineer\n");
-	sleep(1);
-	printf("\n");
-	
-	printf("　　　　　　　　　　　 Jinwoo LEE　Producer & Director\n");
-	sleep(1);
-	printf("　　　　　　　　　　　　　　 　　　Motion Graphic Design\n");
-	sleep(1);
-	printf("　　　　　　　　　　　　　　 　　　Superviser\n");
-	sleep(1);
-	printf("\n");
-	
-	printf("　　　　　　　　　 Hyunjoong YOON　Director\n");
-	sleep(1);
-	printf("　　　　　　　　　　　　　　 　　　Art Design\n");
-	sleep(1);
-	printf("　　　　　　　　　　　　　　 　　　Coordinating\n");
-	printf("\n");
-	printf("\n");
-	sleep(1);
-	printf("　　　　　　　　　　　　　　　　　　Present By Team IoT3...Thank you...\n");
-	sleep(1);
-	sleep(600);
+    printf("\033[2J\033[H"); // Clear screen
+    
+  
+    printf("\n");
+    printf("　　　　　　　　        Sola HONG　Data Manager\n");
+    sleep(1);
+    printf("　　　　　　　　　　　　　　　　 　Subtitle/Translate\n");
+    sleep(1);
+    printf("　　　　　　　　　　　　　　　 　　Line Producer\n");
+    sleep(1);
+    printf("\n");
+    printf("　　　　　　　　　　 Haedeun SONG　Production Assistant\n");
+    sleep(1);
+    printf("　　　　　　　　　　　　　　　 　　Art Design\n");
+    sleep(1);
+    printf("　　　　　　　　　　　　　　　 　　Tech Engineer\n");
+    sleep(1);
+    printf("\n");
+    printf("　　　　　　　　　　　 Jinwoo LEE　Producer & Director\n");
+    sleep(1);
+    printf("　　　　　　　　　　　　　　 　　　Motion Graphic Design\n");
+    sleep(1);
+    printf("　　　　　　　　　　　　　　 　　　Superviser\n");
+    sleep(1);
+    printf("\n");
+    printf("　　　　　　　　　 Hyunjoong YOON　Director\n");
+    sleep(1);
+    printf("　　　　　　　　　　　　　　 　　　Art Design\n");
+    sleep(1);
+    printf("　　　　　　　　　　　　　　 　　　Coordinating\n");
+    printf("\n");
+    sleep(1);
+    printf("　　　　　　　　　　　　　　　　　　Present By Team IoT3, Thank you!\n");
+    sleep(1);
+    printf("\n");
+    printf("               🛸　　　 　🌎　°　　🌓　•　　.°•　　　🚀 ✯\n");
+    printf("　            　★　*　　　　　°　　　　🛰 　°·　　   🪐\n");
+    printf("                           .　　☄ 　•　° ★　 •\n");
+    printf("                             ▁▂▃▄▅▆▇▇▆▅▄▃▁▂\n");
+    
+    sleep(600);
 }
+
 
 //이미지 출력 로직
 void display_images() {
